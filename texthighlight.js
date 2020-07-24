7d63210c1f111f36fa4839ff241d5fd03b775f35
hmenss2s <hajar.menssouri@smail.inf.h-brs.de>
2020-07-24 20:36:46 +0200

    implement comment edit save cancel

--- texthighlight.js
+++ texthighlight.js
@@ -9,6 +9,7 @@
         // ccm: '//ccmjs.github.io/ccm/ccm.js',
 
         config: {
+            db: ["ccm.store", { name: 'ccm-texthighlight' }],
 
             texthighlight: {
                 id: 'texthighlight',
@@ -16,7 +17,7 @@
                     {
                         tag: 'div',
                         id: 'text',
-                        inner: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata'
+                        inner: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimataLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. '
 
 
                     },
@@ -30,7 +31,8 @@
                                     {
                                         tag: 'button',
                                         id: 'comment',
-                                        inner: 'Comment'
+                                        onclick: '%comment%'
+
                                     },
                                     {
                                         tag: 'button',
@@ -62,13 +64,46 @@
                                     {
                                         tag: 'button',
                                         id: 'trash',
-                                        inner: 'Delete',
                                         onclick: '%remove%'
                                     }
 
                                 ]
 
+                            },
+                            {
+                                tag: 'div',
+                                id: 'comment-container',
+                                inner:[
+                                    {
+                                        tag :'textarea',
+                                        id: 'comment-area',
+                                        placeholder: 'Enter comment...'
+
+                                    }
+                                ]
+
+                            },
+                            {
+                                tag: 'button',
+                                id: 'cancel',
+                                inner: 'Cancel',
+                                onclick: '%cancel%'
+
+
+                            },
+                            {
+                                tag: 'button',
+                                id: 'save',
+                                inner: 'Save',
+                                onclick: '%save%'
+                            },
+                            {
+                                tag: 'button',
+                                id: 'edit',
+                                inner: 'EDIT',
+                                onclick: '%edit%'
                             }
+
                         ]
 
                     }
@@ -111,6 +146,7 @@
                 let shadow;
                 let selection;
                 let range;
+                let markElement;
 
 
                 const texthighlight = $.html(self.texthighlight, {
@@ -122,32 +158,69 @@
                         }else{
                             const mark = getMarkElement();
                             mark.style.background = color;
+                            updateComment(mark.id,color,null);
 
                         }
-
+                        setContent(text.innerHTML);
+                        hideMenu();
 
 
                     },
+
+
                     remove: function (e) {
                         deleteMark();
+                        setContent(text.innerHTML);
+                    },
+
+                    comment: function (e) {
+                        showCommentArea();
+
+                    },
+
+                    cancel: function (e){
+                        hideCommentArea();
+                        hideMenu();
+                    },
+
+                    save: function (e) {
+                        saveComment();
+
+                    },
+                    edit: function(e){
+                        const text =texthighlight.querySelector("#comment-area");
+                        const comment = text.value;
+                        editComment(comment);
                     }
 
                 });
 
                 const text = texthighlight.querySelector('#text');
+                getContent(text.innerHTML);
                 text.onmouseup = (e) => {
                     handleSelection();
                 }
+                hideMenu();
                 $.setContent(self.element, texthighlight);
 
                 function handleSelection() {
 
                     shadow = self.element.parentNode;
                     selection = shadow.getSelection();
-                    if (selection) {
-                        range = selection.getRangeAt(0);
-                        console.log(selection.toString());
+                    range = selection.getRangeAt(0);
+                    const edit =texthighlight.querySelector("#edit");
+                    edit.style.display ="none";
+                    if (selection && selection.toString()!== "") {
+                        showMenu();
 
+                    }else {
+                        if (isAlreadyMarked()) {
+                            markElement = getMarkElement();
+                            showMenu();
+                        } else {
+
+                            hideMenu();
+                        }
                     }
 
                 }
@@ -155,6 +228,8 @@
                 function surroundText(color) {
                     const mark = document.createElement("mark");
                     mark.style.background = color;
+                    mark.id = create_UUID();
+
                     mark.appendChild(range.extractContents());
                     range.insertNode(mark);
                     selection.removeAllRanges();
@@ -165,6 +240,8 @@
                     if (isAlreadyMarked()) {
                         const elem = getMarkElement();
                         elem.replaceWith(...elem.childNodes);
+                        hideMenu();
+                        self.db.del(markElement.id);
 
 
                     }
@@ -199,6 +276,178 @@
 
                 }
 
+                function showMenu(){
+                    hideCommentArea();
+                    defaultTextArea();
+                    const menu = texthighlight.querySelector("#contextmenu");
+                    const comment = menu.querySelector("#comment");
+                    const trash = menu.querySelector("#trash");
+                    if(!isAlreadyMarked()){
+                        comment.style.visibility ="hidden";
+                        trash.style.visibility ="hidden";
+                    }else{
+                        comment.style.visibility = "visible";
+                        trash.style.visibility = "visible";
+                        getComment(markElement.id);
+                    }
+                    setMenuPosition();
+                    menu.style.visibility = "visible"
+                }
+
+                function hideMenu(){
+
+                    const menu = texthighlight.querySelector("#contextmenu");
+                    const comment = menu.querySelector("#comment");
+                    const trash = menu.querySelector("#trash");
+
+                    comment.style.visibility ="hidden";
+                    trash.style.visibility ="hidden";
+                    menu.style.visibility = "hidden";
+                    defaultTextArea();
+                    hideCommentArea();
+                }
+
+                function setMenuPosition(){
+                    const clientRect = range.getBoundingClientRect();
+                    const menu = texthighlight.querySelector("#contextmenu");
+                    menu.style.left = clientRect.left + "px";
+                    menu.style.top = (clientRect.bottom -  clientRect.height - menu.clientHeight) + "px";
+
+
+                }
+
+                function showCommentArea(){
+                    const area = texthighlight.querySelector("#comment-container");
+                    const cancel = texthighlight.querySelector("#cancel");
+                    const save = texthighlight.querySelector("#save");
+                    area.style.display = "block";
+                    cancel.style.display = "block";
+                    save.style.display = "block";
+
+
+                }
+
+                function hideCommentArea() {
+                    const area = texthighlight.querySelector("#comment-container");
+                    const cancel = texthighlight.querySelector("#cancel");
+                    const save = texthighlight.querySelector("#save");
+                    const text = texthighlight.querySelector("#comment-area")
+                    area.style.display = "none";
+                    cancel.style.display = "none";
+                    save.style.display = "none";
+                    text.value = "";
+
+
+                }
+
+                function setContent(content){
+                    const KEY ="ccm-content";
+                    self.db.set({key:KEY,value:content});
+
+                }
+                function getContent(content){
+                    const KEY ="ccm-content";
+                    self.db.get(KEY).then(
+
+                        existContent=> {
+                            if(existContent){
+                                text.innerHTML = existContent.value;
+                            }else{
+                                text.innerHTML = content;
+                            }
+                        }
+                    )
+
+                }
+
+                function saveComment() {
+                    const id = markElement.id;
+                    const color = markElement.style.background;
+                    const text = texthighlight.querySelector("#comment-area");
+                    const comment = text.value;
+
+                    const commentObject = {
+                        color : color,
+                        comment : comment
+                    }
+                    self.db.set({key:id,value:commentObject});
+                    hideMenu();
+
+                }
+
+                function getComment(id){
+                    self.db.get(id).then(
+                        comment =>{
+                            if(comment){
+                                const text =texthighlight.querySelector("#comment-area");
+                                text.value = comment.value.comment;
+                                text.style.background = comment.value.color;
+                                text.readOnly = true;
+                                const edit =texthighlight.querySelector("#edit");
+                                edit.style.display ="block";
+
+                                const commentBtn = texthighlight.querySelector("#comment");
+                                commentBtn.style.visibility ="hidden";
+
+                                showCommentArea();
+
+                                const cancel = texthighlight.querySelector("#cancel");
+                                const save = texthighlight.querySelector("#save");
+                                cancel.style.display = "none";
+                                save.style.display = "none";
+                            }else{
+                                const edit =texthighlight.querySelector("#edit");
+                                edit.style.display ="none";
+                            }
+                        }
+                    )
+                }
+
+                function updateComment(id,color,comment) {
+                    self.db.get(id).then(
+                       result=> {
+                           if(result){
+                               if(color){
+                                   result.value.color = color;
+                               }
+                               if(comment){
+                                   result.value.comment = comment;
+                               }
+                               self.db.set({key:id,value:result.value});
+                           }
+                       }
+                    )
+
+
+                }
+
+
+                function editComment(comment){
+                    const text =texthighlight.querySelector("#comment-area");
+                    text.value =comment;
+                    text.readOnly = false;
+                    const edit =texthighlight.querySelector("#edit");
+                    edit.style.display ="none";
+                    const cancel = texthighlight.querySelector("#cancel");
+                    const save = texthighlight.querySelector("#save");
+                    cancel.style.display = "block";
+                    save.style.display = "block";
+
+
+                }
+
+
+                function defaultTextArea(){
+                    const text =texthighlight.querySelector("#comment-area");
+                    text.value ="";
+                    text.style.background = "white";
+                    text.readOnly = false;
+
+                }
+
+
+
+
 
                 /* function removeDummy() {
                   const elem = document.getElementById('trash');
@@ -225,6 +474,15 @@
 
     };
 
+    function create_UUID(){
+        let dt = new Date().getTime();
+        let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (dt + Math.random()*16)%16 | 0;
+            dt = Math.floor(dt/16);
+            return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return 'mark-' + uuid;
+    }
 
     function p() {
         window.ccm[v].component(component)
