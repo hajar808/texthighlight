9c5cefb0a3ca22e922c75068b3a43e05dbda88d9
hmenss2s <hajar.menssouri@smail.inf.h-brs.de>
2020-08-27 22:46:17 +0200

    handle touch screen

--- versions/ccm.texthighlight-1.0.0.js
+++ versions/ccm.texthighlight-1.0.0.js
@@ -218,9 +218,16 @@
                 // init element with current content
                 getContent(text.innerHTML);
 
-                text.onmouseup = (e) => {
-                    handleSelection();
+                if(isTouchScreen()){
+                    text.ontouchend = (e) => {
+                        handleSelection();
+                    }
+                }else{
+                    text.onmouseup = (e) => {
+                        handleSelection();
+                    }
                 }
+
                 // hide context menu if text not selected
                 hideMenu();
 
@@ -254,6 +261,31 @@
                         }
                     }
 
+                }
+
+
+                function isTouchScreen(){
+                    var hasTouchScreen = false;
+                    if ("maxTouchPoints" in navigator) {
+                        hasTouchScreen = navigator.maxTouchPoints > 0;
+                    } else if ("msMaxTouchPoints" in navigator) {
+                        hasTouchScreen = navigator.msMaxTouchPoints > 0;
+                    } else {
+                        var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+                        if (mQ && mQ.media === "(pointer:coarse)") {
+                            hasTouchScreen = !!mQ.matches;
+                        } else if ('orientation' in window) {
+                            hasTouchScreen = true; // deprecated, but good fallback
+                        } else {
+                            // Only as a last resort, fall back to user agent sniffing
+                            var UA = navigator.userAgent;
+                            hasTouchScreen = (
+                                /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+                                /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+                            );
+                        }
+                    }
+                    return hasTouchScreen;
                 }
 
 
